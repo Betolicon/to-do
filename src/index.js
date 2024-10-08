4d2d5bb04d910a773e2a74936639cf4bab137d0f
Betolicon <[albertlicontti@gmail.com]>
2024-10-07 22:43:13 -0400

    I made some changes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
 import "./style.css"
-import { createMenu } from './menu.js';
-import { createContact } from './contact.js';
-import { Home } from './home.js'
-  
-const buttons = document.querySelectorAll('button')
-const content = document.getElementById('content')
 
-window.onload = () => {
-    Home()
-    buttons[0].classList.add('selected');
-};
+const h1 = document.querySelectorAll('h1')
 
-const Selected = (e) => {
-    buttons.forEach(button => button.classList.remove('selected'));
-    e.target.classList.add('selected');
-};
-    
-const renderContent = (e) =>{
-    content.innerHTML = ''
+const message = (e) => console.log(e)
 
-    if(e.target.id === 'menu')
-        createMenu()
-    else if (e.target.id === 'contact')
-        createContact()
-    else
-        Home()
-    Selected(e)
-}
-
-buttons.forEach(button => button.addEventListener('click', renderContent))
+h1.forEach((title) => title.addEventListener('click', message))
