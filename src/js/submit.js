7616c27a9da4eea5ab16780003878adc5347db70
Betolicon <[albertlicontti@gmail.com]>
2024-10-10 22:47:02 -0400

    I created a new file for when a user click the submit button of the form

---
+++ src/js/submit.js
@@ -0,0 +1,9 @@
+export const Submit = () =>{
+
+    const submitted = () =>{
+        alert("Form submitted");
+    }
+
+    const buttonS = document.getElementById('Submit')
+    buttonS.addEventListener('click', submitted)
+}
