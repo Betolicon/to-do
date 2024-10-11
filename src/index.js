55ba63c7004d3a7a634eb0e270ae3432f5fd6de2
Betolicon <[albertlicontti@gmail.com]>
2024-10-10 22:45:00 -0400

    I imported a new function from a js file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import "./style.css"
 import { selected } from './js/selected.js'
 import { photo } from './js/photo.js'
 import { Close } from "./js/closeD.js"
+import { Submit } from "./js/submit.js"
 
 selected()
 photo()
 Close()
+Submit()
