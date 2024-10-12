3693aa7c14bd6deff973451263a5269d06390463
Betolicon <[albertlicontti@gmail.com]>
2024-10-12 16:56:46 -0400

    I changed this one to open both forms

--- src/js/selected.js
+++ src/js/selected.js
@@ -1,4 +1,4 @@
-import { dialog, hideDialog } from "./dialog";
+import { dialog, list } from "./dialog";
 
 export const selected = () =>{
     const h1 = document.querySelectorAll('h1')
@@ -6,7 +6,7 @@ export const selected = () =>{
     const select = (e) => {
         h1.forEach(button => button.classList.remove('selected'));
         e.target.classList.add('selected');
-        dialog()
+        (e.target.id == 'tasks') ? dialog() : list()
     };
 
     h1.forEach((title) => title.addEventListener('click', select))
