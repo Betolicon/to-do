731e6d5c1bb05a32c6002cd83dae765175051480
Betolicon <[albertlicontti@gmail.com]>
2024-10-12 16:54:26 -0400

    I added some functions to show and hide the form for adding a new list

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -3,14 +3,28 @@ export const dialog = () =>{
         let dlg = document.getElementById("form");
         dlg.show();
     }
+    showForm()
+}
 
-
+export const list = () =>{
+    const showForm = () =>{
+        let dlg = document.getElementById("list");
+        dlg.show();
+    }
     showForm()
 }
 
 export const hideDialog = ()=>{
     const closeForm = () =>{
         let dlg = document.getElementById("form");
+        dlg.close();
+    }
+    closeForm()
+}
+
+export const hideList = () =>{
+    const closeForm = () =>{
+        let dlg = document.getElementById("list");
         dlg.close();
     }
     closeForm()
