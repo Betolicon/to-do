f5091de955ff0a7c44e9d3e4ac1d26aee4048786
Betolicon <[albertlicontti@gmail.com]>
2025-05-11 20:57:56 -0400

    I modified the design.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "./style.css"
 import { selectedList } from './js/selected.js'
-import { photo } from './js/photo.js'
+// import { photo } from './js/photo.js'
 import { Close } from "./js/closeD.js"
 import { Submit } from "./js/submit.js"
 
 selectedList()
-photo()
+// photo()
 Close()
 Submit()
